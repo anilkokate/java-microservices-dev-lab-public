fe82cffdbe81f1f0272b4e63fb6d7aa54be0ef61
Anil Kokate <anilkokate2009@gmail.com>
2025-05-03 23:20:48 -0700

    Initial commit

---
+++ command-pattern-poc/src/main/java/com/sia/patterns/command_pattern_poc/CommandPatternPocApplication.java
@@ -0,0 +1,13 @@
+package com.sia.patterns.command_pattern_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandPatternPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandPatternPocApplication.class, args);
+	}
+
+}
---
+++ command-pattern-poc/src/test/java/com/sia/patterns/command_pattern_poc/CommandPatternPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sia.patterns.command_pattern_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandPatternPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
